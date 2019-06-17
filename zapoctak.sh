7b9fbe2aed000a07ee6da1b03801b55bd1dca056
Igneus <michal.kazanos@gmail.com>
2019-06-17 04:09:40 +0200

    broken verzia zapoctaku s grafom historie

--- zapoctak.sh
+++ zapoctak.sh
@@ -29,24 +29,49 @@ do
 	clear
 	cat $c | awk -e '
 	BEGIN{
-		x=10
-		for(i=1;i<=100;i++)
-	                if( ((sqrt(x)-1)/(sqrt(10)-1)-(i-15)) < 1 ){
-				printf x
-				x=x*10
-				}
-                	else
-				printf " "
+		printf "\033[33m" "%-20s", " " "\033[33m"
+		for(i=1;i<=100000;i=(i*10)){
+			x=(sqrt(i/10)-1)/(sqrt(10)-1)
+			for(j=0;j<x; j++)
+                                printf " "
+			printf "\033[33m" "%s", i "\033[33m"
+	               } 
 		printf "\n"
 	}
-	$1 != "1o"{
+	!/^lo:/{
 		x=($4 - $2)
-		printf "%-15s", $1 x
+		printf "\033[33m" "%-20s", $1 x "\033[33m"
+		
+		system("touch temp/"$1)
+		print x > "temp/"$1
+
 		x=(sqrt(x)-1)/(sqrt(10)-1)
 		for(i=0;i<x; i++)
-			printf "|"
+			printf "\033[31m" "|" "\033[31m"
 		printf "\n"
+	}
+	END{
+		printf "\033[37m" "" "\033[37m"
 	}'
+	
+	tail -n 100 "temp/eth0:" | awk -e '
+	BEGIN{
+		RS="~"
+	}
+	
+	{
+		for(i=0;i<100;i++)
+			if((sqrt($i)-1)/(sqrt(10)-1))
+				printf "H"
+			else
+				printf " "
 
+		printf "\n"
+	}
+
+	END{
+		RS="\n"
+	}
+	'
 	sleep 1
 done
