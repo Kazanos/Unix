501f7a2db70a8f0082cde62f62f753d4322e8eda
Igneus <michal.kazanos@gmail.com>
2019-06-18 22:28:46 +0200

    zapoctak s 4 udajmi, grafom pre tieto udaje, a skonci pri stlaceni "q"

--- zapoctak.sh
+++ zapoctak.sh
@@ -3,73 +3,134 @@
 simplifyTo(){
 	 cat /proc/net/dev | while read line
         do
-                echo $line | sed -En 's|(.*: [0-9]*).*|\1|p' >> "$1"
+                echo $line | awk -e '/.*[:]/{print $1" "$2" "$3" "$10" "$11}' >> "$1"
         done
 }
 
 a="$TMP/a"
 b="$TMP/b"
 c="$TMP/c"
+input=""
 
 > "$a"
 simplifyTo "$a"
 sleep 1
 
-while true
+while [ "$input" != "q" ]
 do
 	> $b
 	simplifyTo $b
 
-	paste -d ' ' $a $b > $c
+	join  $a $b > $c
 
 	cat $b > $a
 	
 	clear
 	cat $c | awk -v path="$TMP/" -e '
 	BEGIN{
-		printf "\033[33m" "%-20s", " " "\033[33m"
-		for(i=1;i<=10000;i=(i*10)){
-			x=(sqrt(i/10)-1)/(sqrt(10)-1)
-			for(j=0;j<x; j++)
-                                printf " "
-			printf "\033[33m" "%s", i "\033[33m"
-	               } 
+		printf "\033[33m" "%-8s", "Name"
+		printf "\033[33m" "%-18s", "Bytes In"
+		printf "\033[33m" "%-18s", "Packets In"
+		printf "\033[33m" "%-18s", "Bytes Out"
+		printf "\033[33m" "%-18s", "Packets Out"
 		printf "\n"
 	}
 	!/^lo:/{
-		x=($4 - $2)
-		printf "\033[33m" "%-20s", $1 x "\033[33m"
+		inbytes=($6 - $2)
+		inpackets=($7 - $3)
+		outbytes=($8 - $4)
+		outpackets=($9 - $5)
+		printf "\033[32m" "%-8s", $1
+
+		printf "\033[37m" "%-8s", inbytes
+		ib=log(inbytes)/log(10)
+                for(i=1;i<=10; i++)
+			if(i<ib)
+                        	printf "\033[35m" "|"
+			else
+                                printf " "
+
+		printf "\033[37m" "%-8s", inpackets
+		ip=log(inpackets)
+                for(i=1;i<=10; i++)
+			if(i<ip)
+                        	printf "\033[36m" "|"
+			else
+                                printf " "
+
+		printf "\033[37m" "%-8s", outbytes
+		ob=log(outbytes)/log(10)
+                for(i=1;i<=10; i++)
+			if(i<ob)
+                        	printf "\033[31m" "|"
+			else
+                                printf " "
+
+		printf "\033[37m" "%-8s", outpackets
+		op=log(outpackets)
+                for(i=1;i<=10; i++)
+			if(i<op)
+                        	printf "\033[34m" "|"
+			else
+				printf " "
 		
 		system("touch "path$1)
-		print x >> path$1
+		print inbytes" "inpackets" "outbytes" "outpackets >> path$1
 
-		x=(sqrt(x)-1)/(sqrt(10)-1)
-		for(i=0;i<x; i++)
-			printf "\033[31m" "|" "\033[31m"
 		printf "\n"
 	}
 	END{
 		printf "\033[37m" "" "\033[37m"
 	}'
-	tail -n 50 "$TMP/eth0:" | awk -e '
+
+	name="eth0"
+	tail -n 35 "$TMP/$name:" | awk -v name=$name -e '
 	BEGIN{
 		RS="~"
+		printf "Showing "name" graph\n"
 	}
 	
 	{
 		for(j=10;j>0;j--){
-			for(i=0;i<60;i++)
-				if( (sqrt($i)-1)/(sqrt(10)-1) > j*sqrt(10))
-					printf "\033[35m" "H" "\033[37m"
-				else
-					printf " "
+			for(i=0;i<35;i++)
+				if( (log($(4*i+1))/log(10)) > j)
+					printf "\033[35m" "H"
+				 else
+                                        printf " "
+			
+			printf "     "
 
-			printf "\n"}
+			for(i=0;i<35;i++)
+                                if( (log($(4*i+3))/log(10)) > j)
+                                        printf "\033[31m" "H"
+                                 else
+                                        printf " "
+
+                        printf "\n"}
+
+		for(j=10;j>0;j--){
+			for(i=0;i<35;i++)
+                                if( (log($(4*i+2))) > j)
+                                        printf "\033[36m" "H"
+                                else
+                                        printf " "
+			
+			printf "     "
+
+                        for(i=0;i<35;i++)
+                                if( (log($(4*i+4))) > j)
+                                        printf "\033[34m" "H"
+                                 else
+                                        printf " "
+
+                        printf "\n"}
+
 	}
 
 	END{
 		RS="\n"
+		printf "\033[37m" "" "\033[37m"
 	}
 	'
-	sleep 1
+	read -r -s -n 1 -t 1 input
 done
